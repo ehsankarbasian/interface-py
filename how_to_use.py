54885debf64ccfe191c9ad95c94bcf43a2328f32
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2025-09-18 05:54:11 +0330

    Add: README and how_to_use

---
+++ how_to_use.py
@@ -0,0 +1,27 @@
+from interface import InterfaceBase, interface, concrete
+
+
+@interface
+class Human(InterfaceBase):
+    def talk(self): pass
+    def walk(self): ...
+
+
+@interface
+class MilitaryHuman(Human):
+    def shoot(self): ...
+
+
+@concrete
+class Commander(MilitaryHuman):
+    def talk(self): print("talking")
+    def walk(self): print("walking")
+    def shoot(self): print("shooting")
+
+
+# Raises error:
+# Human()
+# MilitaryHuman()
+
+c = Commander()
+c.talk()
