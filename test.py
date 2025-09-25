e065825f9c8b880d3c9c9519da8100eb2b14b743
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2025-09-25 23:09:27 +0330

    Add 'pass' test to InterfaceInstantiationTestCase

--- test.py
+++ test.py
@@ -31,34 +31,52 @@ class InterfaceInstantiationTestCase(TestCase):
     
     def setUp(self):
         @interface
-        class Level_1_Interface(InterfaceBase): ...
+        class Level_1_Interface_dot(InterfaceBase): ...
         @interface
-        class Level_2_Interface(Level_1_Interface): ...
+        class Level_2_Interface_dot(Level_1_Interface_dot): ...
         @interface
-        class Level_3_Interface(Level_2_Interface): ...
+        class Level_3_Interface_dot(Level_2_Interface_dot): ...
         
-        self.Level_1_Interface = Level_1_Interface
-        self.Level_2_Interface = Level_2_Interface
-        self.Level_3_Interface = Level_3_Interface
+        self.Level_1_Interface_dot = Level_1_Interface_dot
+        self.Level_2_Interface_dot = Level_2_Interface_dot
+        self.Level_3_Interface_dot = Level_3_Interface_dot
+        
+        @interface
+        class Level_1_Interface_pass(InterfaceBase): pass
+        @interface
+        class Level_2_Interface_pass(Level_1_Interface_pass): pass
+        @interface
+        class Level_3_Interface_pass(Level_2_Interface_pass): pass
+        
+        self.Level_1_Interface_pass = Level_1_Interface_pass
+        self.Level_2_Interface_pass = Level_2_Interface_pass
+        self.Level_3_Interface_pass = Level_3_Interface_pass
     
     
     def tearDown(self):
-        del self.Level_1_Interface
-        del self.Level_2_Interface
-        del self.Level_3_Interface
+        del self.Level_1_Interface_dot
+        del self.Level_2_Interface_dot
+        del self.Level_3_Interface_dot
+        
+        del self.Level_1_Interface_pass
+        del self.Level_2_Interface_pass
+        del self.Level_3_Interface_pass
     
     
     def test_no_instance_from_InterfaceBase(self):
         self.assertRaises(Exception, InterfaceBase)
     
     def test_no_instance_from_interface_level_1(self):
-        self.assertRaises(Exception, self.Level_1_Interface)
+        self.assertRaises(Exception, self.Level_1_Interface_dot)
+        self.assertRaises(Exception, self.Level_1_Interface_pass)
     
     def test_no_instance_from_interface_level_2(self):
-        self.assertRaises(Exception, self.Level_2_Interface)
+        self.assertRaises(Exception, self.Level_2_Interface_dot)
+        self.assertRaises(Exception, self.Level_2_Interface_pass)
     
     def test_no_instance_from_interface_level_3(self):
-        self.assertRaises(Exception, self.Level_3_Interface)
+        self.assertRaises(Exception, self.Level_3_Interface_dot)
+        self.assertRaises(Exception, self.Level_3_Interface_pass)
 
 
 if __name__ == "__main__":
