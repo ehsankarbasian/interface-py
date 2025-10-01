007f5284174280a9fc2810498b084903275b0aad
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2025-10-01 17:49:57 +0330

    New version: 1.1.0

--- a.py
+++
@@ -1,9 +0,0 @@
-from interface import interface, concrete, InterfaceBase
-
-
-@interface
-class MyInterface(InterfaceBase):
-    
-    @property
-    def foo(self):
-        return "bar"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="interface-py",
-    version="1.0.0",
+    version="1.1.0",
     author="Ehsan Karbasian",
     author_email="ehsan.karbasian@gmail.com",
     description="A package to define interface in python",
