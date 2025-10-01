1d218f068a2e2a04b7ed4f61bdd9b79f43172312
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2025-10-01 23:29:08 +0330

    Test: concrete empty interface

---
+++ test/concrete/empty_concrete.py
@@ -0,0 +1,51 @@
+import unittest
+from unittest import TestCase
+
+import pathlib
+import sys
+path = str(pathlib.Path(__file__).parent.parent.parent.absolute())
+sys.path.append(path)
+
+from interface import interface, concrete, InterfaceBase
+
+
+class EmptyConcreteTestCase(TestCase):
+    
+    def setUp(self):
+        
+        @interface
+        class EmptyInterface(InterfaceBase):
+            pass
+        
+        self.EmptyInterface = EmptyInterface
+        return super().setUp()
+    
+    
+    def tearDown(self):
+        del self.EmptyInterface
+        return super().tearDown()
+    
+    
+    def test_empty_interface_empty_concrete(self):
+        
+        @concrete
+        class EmptyConcrete(self.EmptyInterface):
+            pass
+    
+    
+    def test_empty_interface_non_empty_concrete(self):
+        
+        @concrete
+        class EmptyConcrete(self.EmptyInterface):
+            x = 10
+            
+            def talk(self):
+                print('hello')
+                
+            @property
+            def state(self):
+                return 'OK'
+
+
+if __name__ == "__main__":
+    unittest.main()
