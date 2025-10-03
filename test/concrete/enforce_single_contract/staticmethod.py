2d0120cb1d2d91d4eae51c607c44c92a6915ce0d
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2025-10-03 17:12:34 +0330

    test encforce concrete staticmethod

---
+++ test/concrete/enforce_single_contract/staticmethod.py
@@ -0,0 +1,73 @@
+import unittest
+from unittest import TestCase
+
+import pathlib
+import sys
+path = str(pathlib.Path(__file__).parent.parent.parent.parent.absolute())
+sys.path.append(path)
+
+from interface import interface, concrete
+
+
+class StaticMethodContractTestCase(TestCase):
+    
+    def setUp(self):
+        
+        @interface
+        class _MyInterface:
+            @staticmethod
+            def foo(par_1, par_2):
+                pass
+        
+        self.MyInterface = _MyInterface
+    
+    
+    def tearDown(self):
+        del self.MyInterface
+        return super().tearDown()
+    
+    
+    def test_success(self):
+        @concrete
+        class MyConcrete(self.MyInterface):
+            @staticmethod
+            def foo(par_1, par_2):
+                return f'{par_1} {par_2}'
+    
+    
+    def test_instantiate_good_concrete(self):
+        @concrete
+        class MyConcrete(self.MyInterface):
+            @staticmethod
+            def foo(par_1, par_2):
+                return f'{par_1} {par_2}'
+        
+        MyConcrete()
+    
+    
+    def test_not_the_same_params(self):
+        expected_message = "Signature mismatch for 'foo' in concrete 'MyConcrete': expected (par_1, par_2), got (par_1)."
+        
+        with self.assertRaises(TypeError) as context:
+            @concrete
+            class MyConcrete(self.MyInterface):
+                @staticmethod
+                def foo(par_1):
+                    return par_1
+        
+        self.assertEqual(str(context.exception), expected_message)
+    
+    
+    def test_no_implement_contract(self):
+        expected_message = "Concrete class 'MyConcrete' must implement contracts: foo"
+        
+        with self.assertRaises(TypeError) as context:
+            @concrete
+            class MyConcrete(self.MyInterface):
+                pass
+            
+        self.assertEqual(str(context.exception), expected_message)
+
+
+if __name__ == "__main__":
+    unittest.main()
