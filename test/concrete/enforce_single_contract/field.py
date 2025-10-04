8ddf0471a785e441c61a80cacde45d7fa75ee6f3
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2025-10-04 12:30:44 +0330

    Test concterer fields

---
+++ test/concrete/enforce_single_contract/field.py
@@ -0,0 +1,76 @@
+import unittest
+from unittest import TestCase
+
+import pathlib
+import sys
+path = str(pathlib.Path(__file__).parent.parent.parent.parent.absolute())
+sys.path.append(path)
+
+from interface import interface, concrete
+
+
+class FieldContractElipsisTestCase(TestCase):
+    
+    def setUp(self):
+        
+        @interface
+        class _MyInterface:
+            x = ...
+        
+        self.MyInterface = _MyInterface
+    
+    
+    def tearDown(self):
+        del self.MyInterface
+        return super().tearDown()
+    
+    
+    def test_success(self):
+        @concrete
+        class MyConcrete(self.MyInterface):
+            x = 5
+    
+    
+    def test_instantiate_good_concrete(self):
+        @concrete
+        class MyConcrete(self.MyInterface):
+            x = 5
+        
+        MyConcrete()
+    
+    
+    def test_no_implement_contract(self):
+        expected_message = "Concrete class 'MyConcrete' must implement contracts: x"
+        
+        with self.assertRaises(TypeError) as context:
+            @concrete
+            class MyConcrete(self.MyInterface):
+                pass
+            
+        self.assertEqual(str(context.exception), expected_message)
+
+
+class FieldContractAnnotationTestCase(FieldContractElipsisTestCase):
+
+    def setUp(self):
+        
+        @interface
+        class _MyInterface:
+            x: int
+
+        self.MyInterface = _MyInterface
+
+
+class FieldContractElipsisAndAnnotationTestCase(FieldContractElipsisTestCase):
+
+    def setUp(self):
+        
+        @interface
+        class _MyInterface:
+            x: int = ...
+
+        self.MyInterface = _MyInterface
+
+
+if __name__ == "__main__":
+    unittest.main()
