112540a8a287f19527777a9ba2b31332811340e5
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2025-10-08 14:01:34 +0330

    Debug: benchmark tests

--- test/benchmark/class_generator.py
+++ test/benchmark/class_generator.py
@@ -10,62 +10,90 @@ from abc import ABC, abstractmethod
 @interface
 class FooInterface:
     x: int
+    
     def do(self): ...
+    
     @staticmethod
     def s(): ...
+    
     @property
     def y(self): ...
 
 
 @concrete
-class ConcreteFooInterface(FooInterface):
+class ConcreteFoo(FooInterface):
     x: int = 10
-    def do(self): return 42
+    
+    def do(self):
+        return 42
+    
     @staticmethod
-    def s(): return "ok"
+    def s():
+        return "ok"
+    
     @property
-    def y(self): return "y"
+    def y(self):
+        return "y"
 
 
 class AbstractFoo(ABC):
     @abstractmethod
     def do(self): ...
+    
     @abstractmethod
     def s(self): ...
+    
     @property
     @abstractmethod
     def y(self): ...
 
+
 class ConcreteFooABC(AbstractFoo):
-    def do(self): return 42
-    def s(self): return "ok"
+    
+    def do(self):
+        return 42
+    
+    def s(self): 
+        return "ok"
+    
     @property
-    def y(self): return "y"
+    def y(self):
+        return "y"
 
 
 def make_interface_class():
+    
     @interface
     class FakeInterface:
         a: int
+        
         def f(self): ...
+        
     return FakeInterface
 
 
 def make_concrete_class():
-    @concrete
-    class FakeConcrete(ConcreteFooInterface):
+    
+    class FakeConcrete(ConcreteFoo):
         pass
+    
     return FakeConcrete
 
 
 def make_abc_class():
+    
     class FakeABC(ABC):
+        
         @abstractmethod
         def f(self): ...
+        
     return FakeABC
 
 
 def make_abc_impl():
     class ConcreteFakeABC(make_abc_class()):
-        def f(self): return 1
+        
+        def f(self):
+            return 1
+        
     return ConcreteFakeABC
