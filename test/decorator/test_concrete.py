8c9f515ebe4c9c34178bf60170407b0eb0e1eff2
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2025-10-08 14:40:27 +0330

    Test decorator errors behaviour

---
+++ test/decorator/test_concrete.py
@@ -0,0 +1,96 @@
+import unittest
+from unittest import TestCase
+
+import pathlib
+import sys
+path = str(pathlib.Path(__file__).parent.parent.parent.absolute())
+sys.path.append(path)
+
+from interface import interface, concrete
+
+
+class ـNonInterfaceParent:
+    pass
+
+
+class TestConcreteDecorator(TestCase):
+
+    def test_valid_concrete_with_one_only_interface_parent(self):
+        
+        @interface
+        class MyInterface:
+            ...
+
+        @concrete
+        class MyConcrete(MyInterface):
+            ...
+
+        self.assertFalse(getattr(MyConcrete, "_is_interface_"))
+    
+    
+    def test_valid_concrete_with_one_interface_parents(self):
+        
+        @interface
+        class MyInterface:
+            ...
+
+        @concrete
+        class MyConcrete(MyInterface, ـNonInterfaceParent):
+            ...
+
+        self.assertFalse(getattr(MyConcrete, "_is_interface_"))
+    
+    
+    def test_valid_concrete_with_multiple_only_interface_parents(self):
+        
+        @interface
+        class MyInterface_1:
+            ...
+        
+        @interface
+        class MyInterface_2:
+            ...
+
+        @concrete
+        class MyConcrete(MyInterface_1, MyInterface_2):
+            ...
+
+        self.assertFalse(getattr(MyConcrete, "_is_interface_"))
+    
+    
+    def test_valid_concrete_with_multiple_interface_parents(self):
+        
+        @interface
+        class MyInterface_1:
+            ...
+        
+        @interface
+        class MyInterface_2:
+            ...
+
+        @concrete
+        class MyConcrete_1(MyInterface_1, ـNonInterfaceParent, MyInterface_2):
+            ...
+        
+        @concrete
+        class MyConcrete_2(ـNonInterfaceParent, MyInterface_1, MyInterface_2):
+            ...
+
+        self.assertFalse(getattr(MyConcrete_1, "_is_interface_"))
+        self.assertFalse(getattr(MyConcrete_2, "_is_interface_"))
+
+
+    def test_invalid_concrete_without_interface_parent(self):
+        expected_message = "Concrete class 'BadConcrete' must inherit from at least one interface."
+        
+        with self.assertRaises(TypeError) as context:
+            
+            @concrete
+            class BadConcrete(ـNonInterfaceParent):
+                ...
+        
+        self.assertEqual(str(context.exception), expected_message)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ test/decorator/test_interface.py
@@ -0,0 +1,114 @@
+import unittest
+from unittest import TestCase
+
+import pathlib
+import sys
+path = str(pathlib.Path(__file__).parent.parent.parent.absolute())
+sys.path.append(path)
+
+from interface import interface, concrete
+
+
+class ـNonInterfaceParent:
+    pass
+
+
+class TestInterfaceDecorator(TestCase):
+
+    def test_valid_single_interface_inheritance(self):
+        
+        @interface
+        class ParentInterface:
+            ...
+
+        @interface
+        class ChildInterface(ParentInterface):
+            ...
+
+        self.assertTrue(getattr(ChildInterface, "_is_interface_"))
+    
+    
+    def test_valid_multiple_interface_inheritance(self):
+        
+        @interface
+        class ParentInterface_1:
+            ...
+        
+        @interface
+        class ParentInterface_2:
+            ...
+
+        @interface
+        class ChildInterface(ParentInterface_1, ParentInterface_2):
+            ...
+
+        self.assertTrue(getattr(ChildInterface, "_is_interface_"))
+    
+    
+    def test_valid_chained_interface_inheritance(self):
+        
+        @interface
+        class ParentInterface:
+            ...
+        
+        @interface
+        class MiddleInterface(ParentInterface):
+            ...
+
+        @interface
+        class ChildInterface(MiddleInterface):
+            ...
+
+        self.assertTrue(getattr(MiddleInterface, "_is_interface_"))
+        self.assertTrue(getattr(ChildInterface, "_is_interface_"))
+    
+    
+    def test_valid_multiple_chained_interface_inheritance(self):
+        
+        @interface
+        class ParentInterface_1:
+            ...
+        
+        @interface
+        class ParentInterface_2:
+            ...
+        
+        @interface
+        class ParentInterface_3:
+            ...
+        
+        @interface
+        class ParentInterface_4:
+            ...
+        
+        @interface
+        class MiddleInterface_1(ParentInterface_1, ParentInterface_2):
+            ...
+        
+        @interface
+        class MiddleInterface_2(ParentInterface_3, ParentInterface_4):
+            ...
+
+        @interface
+        class ChildInterface(MiddleInterface_1, MiddleInterface_2):
+            ...
+
+        self.assertTrue(getattr(MiddleInterface_1, "_is_interface_"))
+        self.assertTrue(getattr(MiddleInterface_2, "_is_interface_"))
+        self.assertTrue(getattr(ChildInterface, "_is_interface_"))
+
+
+    def test_invalid_interface_inheritance(self):
+        expected_message = "In interface 'BadInterface', all parents must be interfaces. Found non-interface parent 'ـNonInterfaceParent'."
+        
+        with self.assertRaises(TypeError) as context:
+            
+            @interface
+            class BadInterface(ـNonInterfaceParent):
+                ...
+        
+        self.assertEqual(str(context.exception), expected_message)
+
+
+if __name__ == "__main__":
+    unittest.main()
