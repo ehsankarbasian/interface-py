1ee30f0ec623954b5153933849ab198182ff0caa
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2025-10-12 12:50:59 +0330

    Release new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="interface-py",
-    version="1.3.3",
+    version="1.3.4",
     author="Ehsan Karbasian",
     author_email="ehsan.karbasian@gmail.com",
     description="A package to define interface in python",
