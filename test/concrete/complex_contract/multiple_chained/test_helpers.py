f3d45bf0959035033bb6c8222c7e9fffbc98229a
Ehsan Karbasian <ehsan.karbasian@gmail.com>
2025-10-14 03:12:24 +0330

    Debug and Refactor test_helpers

--- test/concrete/complex_contract/multiple_chained/test_helpers.py
+++ test/concrete/complex_contract/multiple_chained/test_helpers.py
@@ -20,12 +20,6 @@ class _Interface_1:
 class _Interface_2(_Interface_1):
     
     def method_2(self, param_1): ...
-    
-    @classmethod
-    def class_method_2(cls): pass
-    
-    @property
-    def property_2(self): ...
 
 
 @interface
@@ -33,26 +27,20 @@ class _Interface_3:
     
     def method_3(self): pass
     
-    @property
-    def property_3(self): ...
+    @staticmethod
+    def static_method_3(param_1): pass
 
 
 @interface
 class _Interface_4(_Interface_3):
     field_4: bool = ...
-    
-    @staticmethod
-    def static_method_4(param_1, param_2): pass
-    
-    @property
-    def property_4(self): ...
 
 
 @interface
 class _Interface_5(_Interface_2, _Interface_3):
-    field_5: float
     
-    def method_5(self, a, b): pass
+    @classmethod
+    def class_method_5(cls, a, b): pass
     
     @property
     def property_5(self): """Docstring only"""
@@ -73,16 +61,11 @@ class _Interface_7(_Interface_6):
     
     @staticmethod
     def static_method_7(param_1): pass
-    
-    @property
-    def property_7(self): """Docstring only"""
 
 
 @interface
 class _Interface_8(_Interface_4, _Interface_7):
     field_8: dict = ...
-    
-    def method_8(self, x): """Docstring only"""
     
     @property
     def property_8(self): pass
@@ -93,21 +76,18 @@ class _Interface_9(_Interface_5, _Interface_6):
     field_9: complex
     
     def method_9(self): ...
-    
-    @property
-    def property_9(self): pass
 
 
 __INTERFACE_CONTRACTS = {
     1: ['field_1', 'property_1'],
-    2: ['method_2', 'class_method_2', 'property_2'],
-    3: ['method_3', 'property_3'],
-    4: ['field_4', 'static_method_4', 'property_4'],
-    5: ['field_5', 'method_5', 'property_5'],
+    2: ['method_2'],
+    3: ['method_3', 'static_method_3'],
+    4: ['field_4'],
+    5: ['class_method_5', 'property_5'],
     6: ['field_6', 'property_6'],
-    7: ['method_7', 'static_method_7', 'property_7'],
-    8: ['field_8', 'method_8', 'property_8'],
-    9: ['field_9', 'method_9', 'property_9']
+    7: ['method_7', 'static_method_7'],
+    8: ['field_8', 'property_8'],
+    9: ['field_9', 'method_9']
 }
 
 def __get_contract_aggregate(*indexes):
@@ -139,42 +119,48 @@ _AGGREGATED_CONTRACTS = {
 class ContractEnforceTestCase(TestCase):
     
     def setUp(self):
-        # self.Interface_1 = _Interface_1
-        self.Interface_2 = _Interface_2
-        # self.Interface_3 = _Interface_3
-        self.Interface_4 = _Interface_4
-        self.Interface_5 = _Interface_5
-        # self.Interface_6 = _Interface_6
-        self.Interface_7 = _Interface_7
-        self.Interface_8 = _Interface_8
-        self.Interface_9 = _Interface_9
+        self.Interface_2_4 = (_Interface_2, _Interface_4)
+        self.Interface_2_4_7 = (_Interface_2, _Interface_4, _Interface_7)
+        self.Interface_5_7 = (_Interface_5, _Interface_7)
+        self.Interface_5_8 = (_Interface_5, _Interface_8)
+        self.Interface_4_9 = (_Interface_4, _Interface_9)
+        self.Interface_4_7_9 = (_Interface_4, _Interface_7, _Interface_9)
     
     
     def tearDown(self):
-        # del self.Interface_1
-        del self.Interface_2
-        # del self.Interface_3
-        del self.Interface_4
-        del self.Interface_5
-        # del self.Interface_6
-        del self.Interface_7
-        del self.Interface_8
-        del self.Interface_9
+        del self.Interface_2_4
+        del self.Interface_2_4_7
+        del self.Interface_5_7
+        del self.Interface_5_8
+        del self.Interface_4_9
+        del self.Interface_4_7_9
+    
+    
+    @property
+    def current_interfaces(self):
+        return self._current_interfaces
+    
+    @current_interfaces.setter
+    def current_interfaces(self, value):
+        assert isinstance(value, tuple)
+        self._interface_indexes = _HashableList([
+            int(item.__name__.split('_')[-1]) for item in value])
+        self._current_interfaces = value
+    
+    @current_interfaces.deleter
+    def current_interfaces(self):
+        del self._interface_indexes
+        del self._current_interfaces
 
 
 class ContractEnforceMultipleChainedTestCase(ContractEnforceTestCase):
     
     expected_message_prefix: str = ...
-    # self.current_interfaces: list[int]
+    # self.current_interfaces: self.Interface_n_m
     
     def setUp(self):
         self._assertExpectedMessagePrefixSettedCorrectly()
         return super().setUp()
-    
-    def __setattr__(self, name, value):
-        if name == "current_interfaces":
-            value = _HashableList(value)
-        return super().__setattr__(name, value)
     
     def tearDown(self):
         self._assertCurrentInterfaces()
@@ -187,18 +173,18 @@ class ContractEnforceMultipleChainedTestCase(ContractEnforceTestCase):
         self.assertIsInstance(self.expected_message_prefix, str)
     
     def _assertCurrentInterfaces(self):
-        self.assertTrue(hasattr(self, 'current_interfaces'))
+        self.assertTrue(hasattr(self, '_current_interfaces'))
         self.assertIsNotNone(self.current_interfaces)
         self.assertNotEqual(self.current_interfaces, Ellipsis)
-        self.assertIsInstance(self.current_interfaces, list)
-        for item in self.current_interfaces:
+        self.assertIsInstance(self.current_interfaces, tuple)
+        for item in self._interface_indexes:
             self.assertIsInstance(item, int)
             self.assertGreater(item, 0)
             self.assertGreaterEqual(9, item)
     
     @property
     def _aggregated_contracts(self):
-        return _AGGREGATED_CONTRACTS[self.current_interfaces]
+        return _AGGREGATED_CONTRACTS[self._interface_indexes]
 
 
     def assertContractError(self, context, expected_contracts):
